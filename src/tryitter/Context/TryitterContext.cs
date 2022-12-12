5c935f5917779e2328397f8ab917b4ece684df03
viniciusOP <viniciusopassos@hotmail.com>
2022-12-12 15:30:04 -0300

    feat: criar tryitter context

---
+++ src/tryitter/Context/TryitterContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace tryitter.Context
+{
+    public class TryitterContext : DbContext
+    {
+        public TryitterContext(DbContextOptions<TryitterContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students{ get; set; }
+        public DbSet<Post> Posts{ get; set; }
+    }
+}
