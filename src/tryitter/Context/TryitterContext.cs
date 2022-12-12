a5cccf2efaf8369230e43aa5116bbbf8f1b18d55
viniciusOP <viniciusopassos@hotmail.com>
2022-12-12 15:53:01 -0300

    feat: configurando a conexão

--- src/tryitter/Context/TryitterContext.cs
+++ src/tryitter/Context/TryitterContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using tryitter.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace tryitter.Context
--- src/tryitter/Program.cs
+++ src/tryitter/Program.cs
@@ -1,6 +1,11 @@
+using tryitter.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TryitterContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
